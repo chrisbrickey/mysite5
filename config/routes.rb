507d6fc34ca8c6601e65244d778411164a24d190
chrisbrickey <cbrickey1@gmail.com>
2017-04-05 18:18:19 -0700

    Added table for projects and updated look/feel

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :projects
   root 'welcome#projects', as: 'welcome_projects'
 
   get 'welcome/projects'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Project.delete_all
+
+Project.create(title: 'GoTango App',
+  description: "UX design for custom app.\nInsert more detailed description here.",
+  image_url: 'tango_cover.jpeg',
+  hyperlink: 'https://github.com/chrisbrickey'
+)
